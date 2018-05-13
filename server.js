cbcf759d8b82a013af3751598f7444416001ab9f
Zach Roof <zroof85@gmail.com>
2018-05-13 09:05:28 -0400

    Updating for persistent-xss-1

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.use('/encryptionkeys/:file', keyServer())
 /* Swagger documentation for B2B v2 endpoints */
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.use(express.static(applicationRoot + '/app'))
+app.use(express.static(applicationRoot))
 app.use(cookieParser('kekse'))
 app.use(bodyParser.json())
 
